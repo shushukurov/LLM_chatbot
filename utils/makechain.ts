35b8f4e7d0a7dd6eb114379fa09fa786fe508deb
Shakhzod Shukurov <48359297+shushukurov@users.noreply.github.com>
2023-08-16 12:03:53 +0100

    Add files via upload

---
+++ utils/makechain.ts
@@ -0,0 +1,40 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end in simple and accessible way. 
+Provide 3 suggested questions worth asking based on the context.
+If you don't know the answer based on context, just say 'No relevant answer in context' and provide your own answer.
+If the question is not related to the context, respond 'Out of context'.
+
+{context}
+
+Question: {question}
+Helpful answer in markdown and suggested questions:`;
+
+// Generate more questions from the same context
+
+export const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 0, // increase temepreature to get more creative answers
+    modelName: 'gpt-4', //change this to gpt-4 if you have access
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+    },
+  );
+  return chain;
+};
